7efff38e63755c1c1b82399ca3b418515122d0ee
marianomorales <mariano.morales@autostrefa.mx>
2025-10-22 03:58:29 -0600

    Fix: Add vite-tsconfig-paths to resolve module aliases

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
   base: '/',
-  plugins: [react()],
+  plugins: [react(), tsconfigPaths()],
   server: {
     port: 5173,
     proxy: {
